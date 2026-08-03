d9ca446bf5480d266c86749ac29de3815016d8d1
Durvankur Tibile <durvankurtibile1017@gmail.com>
2026-08-03 16:29:12 +0530

    Time: 0 ms (100%), Space: 24 MB (19.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> temp; 
+        int j = -1; 
         for(int i = 0; i<nums.size(); i++){
-            if(nums[i] != 0){
-                temp.push_back(nums[i]); 
+            if(nums[i] == 0){
+                j = i; 
+                break; 
             }
         }
 
-        for(int i = 0 ; i<temp.size(); i++){
-            nums[i] = temp[i]; 
-        }
-
-        for(int i = temp.size(); i<nums.size() ; i++){
-            nums[i] = 0; 
+        for(int i = j; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i],nums[j]); 
+                j++; 
+            }
         }
     }
 };
