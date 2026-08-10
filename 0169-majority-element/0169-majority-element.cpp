18a5e1267a6f9217bf1d20bc6f1fadc7d27490b8
Durvankur Tibile <durvankurtibile1017@gmail.com>
2026-08-10 17:00:10 +0530

    Time: 0 ms (100%), Space: 28.2 MB (74.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,22 +1,21 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        unordered_map<int, int> mpp;
-        for(int x : nums) {
-            mpp[x]++;
-        }
-
-        int maxFreq = 0;
-        int element = -1;
-
-        for(auto it : mpp) {
-            if(it.second > maxFreq) {
-                maxFreq = it.second;
-                element = it.first;
+        int element; 
+        int cnt = 0; 
+        for(int i = 0; i<nums.size(); i++){
+            if(cnt == 0){
+                element = nums[i];
+                cnt++;
+            }
+            else if(nums[i] == element){
+                cnt++; 
+            }
+            else{
+                cnt--; 
             }
         }
-
-        return element;
+        return element; 
     }
 };
