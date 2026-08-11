392149fa6dc5245e2bffcbb193895ad3633bfe7b
Durvankur Tibile <durvankurtibile1017@gmail.com>
2026-08-11 11:04:03 +0530

    Time: 0 ms (100%), Space: 97.4 MB (25.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,8 @@ public:
         int profit = 0; 
         int mini = prices[0]; 
 
-        for(int i = 0; i<prices.size() ; i++){
-            int cost = prices[i] - mini;  
-            profit = max(cost,profit); 
+        for(int i = 0; i<prices.size() ; i++){  
+            profit = max(prices[i] - mini,profit); 
             mini = min(prices[i],mini); 
         }
         return profit; 
