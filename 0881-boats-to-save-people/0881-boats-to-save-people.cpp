d57a6e553d635365e4ea54097f514bc510dfc012
Durvankur Tibile <durvankurtibile1017@gmail.com>
2026-08-22 23:34:52 +0530

    Time: 11 ms (91.23%), Space: 45.8 MB (38.73%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-
-        sort(people.begin(), people.end());
-
+        sort(people.begin(),people.end()); 
         int left = 0;
-        int right = people.size() - 1;
-        int boats = 0;
+        int right = people.size() - 1; 
 
-        while(left <= right) {
+        int cs = 0;        
+        int boats = 0; 
 
-            if(people[left] + people[right] <= limit) {
+        while(left<=right){
+            cs = people[left] + people[right];
+
+            if(cs<=limit){
+                boats++;
                 left++;
+                right--;
             }
 
-            right--;
-            boats++;
+            else{
+                if(people[right]<=limit){
+                    boats++;
+                }
+                right--; 
+            }
         }
-
-        return boats;
+        return boats; 
     }
 };
